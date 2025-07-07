66e025966b518343e3022727263d5fc447f4089f
cole-451 <coleneely87@gmail.com>
2025-07-07 10:07:18 -0600

    Began repacking of SDL code into functions in Renderer.h and Renderer.cpp

---
+++ Source/Engine/Renderer/Renderer.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+#include <SDL3/SDL.h>
+
+
+class Renderer
+{
+public:
+	Renderer() = default;
+
+	bool initialize();
+
+	bool createWindow(const std::string& name, int width, int height);
+
+	void setColor(Uint8 r, Uint8 g, Uint8 b, Uint8  a = 185);
+
+	void clear();
+
+	void present();
+
+	void GTFO();
+
+	void drawline(float x1, float y1, float x2, float y2);
+
+	void drawdot(float x, float y);
+
+private:
+	SDL_Window* current_window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+};
+
+
--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -1,26 +1,18 @@
 #include <SDL3/SDL.h>
 #include <iostream>
 #include "Core/random.h"
+#include "Renderer/Renderer.h"
 
 using namespace parabellum;
 
 int main(int argc, char* argv[]) {
+	Renderer renderer;
     SDL_Init(SDL_INIT_VIDEO);
+	renderer.initialize();
 
-    SDL_Window* window = SDL_CreateWindow("SDL3 Project", 1280, 1024, 0);
-    if (window == nullptr) {
-        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return 1;
-    }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
-    if (renderer == nullptr) {
-        std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
+	renderer.createWindow("SDL3 Project", 1280, 1024);
+   
 
     SDL_Event e;
     bool quit = false;
@@ -38,32 +30,21 @@ int main(int argc, char* argv[]) {
 
 
        
+		renderer.setColor(0, 0, 0, 255); // Set render draw color to black
+		renderer.clear(); // Clear the renderer
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Set render draw color to black
-        SDL_RenderClear(renderer); // Clear the renderer
+		renderer.setColor(0, 255, 0, 255); // Set render draw color to green
+		renderer.clear(); // Clear the renderer
 
-        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // Set render draw color to green
-        SDL_RenderFillRect(renderer, &greenSquare); // Render the rectangle
+		renderer.present(); // Render the screen
+        //SDL_RenderFillRect(renderer, &greenSquare); // Render the rectangle
 
-        // draw some dots
-        for (int i = 0; i < 20; i++) {
-			SDL_SetRenderDrawColor(renderer, random::getrandom(0, 255), random::getrandom(0, 255), random::getrandom(0, 255), random::getrandom(0, 255)); // Set render draw color to a random color
-			// Render a point at a random position
-			SDL_RenderPoint(renderer, random::getrandom(0, 1280), random::getrandom(0, 1024));
-        }
-        //draw some lines
-        for (int i = 0; i < 10; i++) {
-            SDL_SetRenderDrawColor(renderer, random::getrandom(0, 255), random::getrandom(0, 255), random::getrandom(0, 255), random::getrandom(0, 255)); // Set render draw color to a random color
-			SDL_RenderLine(renderer, random::getrandom(0, 1280), random::getrandom(0, 1024), random::getrandom(0, 1280), random::getrandom(0, 1024));
-        }
 
-        SDL_RenderPresent(renderer); // Render the screen
+        renderer.present(); // Render the screen
 
     }
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+	renderer.GTFO(); // Clean up and exit
 
     return 0;
 }
